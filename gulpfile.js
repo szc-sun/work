0a4ce424ae38c9535a03e50757ce2ebef9a974f4
szc-sun <1070446210@qq.com>
2018-06-30 19:07:23 +0800

    6.30下午更新，页面样式

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var rename = require('gulp-rename');
 var cssnano = require("gulp-cssnano");
 
 gulp.task('sass',function(){
-	gulp.src('./src/scss/listpage.scss').pipe(sass()).pipe(gulp.dest('./dist/css'));
+	gulp.src('./src/scss/*.scss').pipe(sass()).pipe(gulp.dest('./dist/css'));
 })
 gulp.task('default',function(){
-	gulp.watch(['./src/scss/.*scss'],['scss']);
+	gulp.watch(['./src/scss/*.scss'],['sass']);
 })
